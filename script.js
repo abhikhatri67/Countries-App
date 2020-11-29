3e5179af7649e238f6d1710ee670f257773c3902
abhikhatri67 <abhikhatri67@gmail.com>
2020-11-29 17:17:29 +0530

    Added countries list page.

---
+++ script.js
@@ -0,0 +1,75 @@
+const countryContainerEl = document.querySelector(".countries-container");
+const regionSelectorEl = document.querySelector(".region-selector");
+
+countries = [];
+regions = [];
+
+async function getCountries() {
+  const resp = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await resp.json();
+
+  return countries;
+}
+
+function getRegionsFromCountries(countries) {
+  const regions = countries.map((country) => country.region);
+  return Array.from(new Set(regions));
+}
+
+function renderCountries(countries) {
+  countryContainerEl.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+
+  countries.forEach((country) => {
+    const countryCardEl = document.createElement("div");
+    countryCardEl.classList.add("country-card");
+
+    countryCardEl.innerHTML = `
+        <div class="card-header">
+          <img class="flag-img" src="${country.flag}" alt="" />
+        </div>
+        <div class="card-content">
+          <div class="country-name">${country.name}</div>
+          <div class="capital">Capital: ${country.capital}</div> 
+          <div class="population">Population: ${country.population.toLocaleString()}</div> 
+        </div>
+  `;
+
+    fragment.appendChild(countryCardEl);
+  });
+
+  countryContainerEl.appendChild(fragment);
+}
+
+function renderRegions(regions) {
+  regions.forEach((region) => {
+    const option = document.createElement("option");
+    option.innerText = region;
+    regionSelectorEl.appendChild(option);
+  });
+}
+
+function filterCountries(event) {
+  const value = event.target.value.toLowerCase();
+  const filterCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(value)
+  );
+  renderCountries(filterCountries);
+}
+
+function filterCountriesByRegion(event) {
+  const value = event.target.value;
+  const filterCountries = countries.filter(
+    (country) => country.region === value
+  );
+  renderCountries(filterCountries);
+}
+
+async function init() {
+  countries = await getCountries();
+  regions = getRegionsFromCountries(countries);
+  renderRegions(regions);
+  renderCountries(countries);
+}
+
+init();
